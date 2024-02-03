e4a67ca02a4e9c50926b9bb8bb62a0e0911306a6
Prashant Shrestha <contact@prashant.me>
2024-02-03 17:15:45 -0500

    Basic layout and functionality completed.

--- src/App.js
+++ src/App.js
@@ -1,55 +1,99 @@
-import { useState } from 'react'
-import './styles/main.css'
+import { useState } from 'react';
+import './styles/main.css';
 import {
     Button,
-    Card,
     ControlGroup,
     FormGroup,
-    H3,
     Icon,
-    InputGroup,
     NumericInput,
     Section,
     SectionCard,
     Slider,
-    Spinner,
     Switch,
     Text,
-} from '@blueprintjs/core'
+} from '@blueprintjs/core';
 
 // or using a ESM bundler which resolves CSS files as modules:
-import 'normalize.css'
-import '@blueprintjs/core/lib/css/blueprint.css'
+import 'normalize.css';
+import '@blueprintjs/core/lib/css/blueprint.css';
 // include blueprint-icons.css for icon font support
-import '@blueprintjs/icons/lib/css/blueprint-icons.css'
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 
 function App() {
-    const [darkTheme, setDarkTheme] = useState(true)
-    const [yardsToMOA, setYardsToMOA] = useState(100)
-    const [clicksPerMOA, setClicksPerMOA] = useState(null)
-    const [yardsToTarget, setYardsToTarget] = useState(null)
-    const [inchesOffTarget, setinchesOffTarget] = useState(null)
-    const [clicks, setClicks] = useState(0)
+    const [darkTheme, setDarkTheme] = useState(true);
+    const [yardsToMOA, setYardsToMOA] = useState(100);
+    const [clicksPerMOA, setClicksPerMOA] = useState(null);
+    const [otherValuesToTarget, setotherValuesToTarget] = useState({
+        feet: 0,
+        meter: 0,
+    });
+    const [yardsToTarget, setYardsToTarget] = useState(null);
+    const [inchesOffTarget, setinchesOffTarget] = useState(null);
+    const [clicks, setClicks] = useState(0);
 
-    function calculateMOA(_inchesOffTarget) {
-        const safeToCalculate = clicksPerMOA && yardsToTarget
+    function calculateClicks() {
+        if (
+            clicksPerMOA &&
+            yardsToTarget &&
+            inchesOffTarget &&
+            typeof clicksPerMOA == 'number' &&
+            typeof yardsToTarget == 'number' &&
+            typeof inchesOffTarget == 'number' &&
+            inchesOffTarget > 0 &&
+            clicksPerMOA > 0
+        ) {
+            const _inchesOffTarget = parseFloat(inchesOffTarget);
+            const inchesPerMOAAdjustment = yardsToTarget / 100;
+            const MOA = _inchesOffTarget / inchesPerMOAAdjustment;
+            const clicksOnScope = clicksPerMOA * MOA;
 
-        if (!safeToCalculate) {
+            if (
+                clicksOnScope &&
+                typeof clicksOnScope == 'number' &&
+                clicksOnScope >= 0
+            ) {
+                setClicks(clicksOnScope);
+            } else {
+                console.log(
+                    `There was issue calculating clicks required in the MOA. ${clicksOnScope}`
+                );
+            }
+        } else {
+            console.log(
+                `Verifying the inputs failed! Please fix the issues first.`
+            );
         }
     }
 
-    function calculateClicks(val) {
-        const _inchesOffTarget = parseFloat(val)
-        const inchesPerMOAAdjustment = yardsToTarget / 100
-        const MOA = _inchesOffTarget / inchesPerMOAAdjustment
-        const clicksOnScope = clicksPerMOA * MOA
+    const _yardsToTarget = (valueInNumber, valueInString) => {
+        if (
+            (valueInNumber && typeof valueInNumber == 'number') ||
+            valueInNumber === 0
+        ) {
+            setYardsToTarget(valueInNumber);
 
-        if (clicksOnScope) {
-            setClicks(clicksOnScope)
+            let conversion = {
+                feet: 3,
+                meter: 0.9144,
+            };
+
+            let converted = {
+                feet: conversion.feet * valueInNumber,
+                meter: conversion.meter * valueInNumber,
+            };
+
+            setotherValuesToTarget(converted);
+            console.log(valueInNumber, converted);
         }
+    };
 
-        // console.log(_inchesOffTarget, clicksOnScope, inchesPerMOAAdjustment, MOA)
-    }
+    const resetFields = () => {
+        // setYardsToMOA(100)
+        setClicksPerMOA('');
+        setYardsToTarget('');
+        setinchesOffTarget('');
+        setClicks(0);
+    };
 
     return (
         <div
@@ -63,23 +107,20 @@ function App() {
                     onClick={() => setDarkTheme(!darkTheme)}
                 />
                 <Section
-                    title={'Scope Calculation'}
-                    subtitle={
-                        'Assists with zero-ing your Red Dot Sight using MOA.'
-                    }
+                    title={'Zero your Red Dot Sight'}
+                    subtitle={'Assists with zero-ing your Red Dot Sight.'}
                     icon="ammunition"
-                    elevation={1}
+                    elevation={2}
                     collapsible={false}
                 >
                     <SectionCard padded={true}>
                         <FormGroup
                             helperText={`1 MOA is ${
                                 (yardsToMOA / 100) * 1.047
-                            } for ${yardsToMOA} yards. Floored: ${Math.floor(
+                            } inches for ${yardsToMOA} yards. Floored: ${Math.floor(
                                 (yardsToMOA / 100) * 1.047
-                            ).toFixed(2)}`}
+                            ).toFixed(2)} inches`}
                             label="Play around (not required for calculation)"
-                            labelFor="playground"
                         >
                             <Slider
                                 initialValue={100}
@@ -96,33 +137,28 @@ function App() {
                         <ControlGroup vertical={false}>
                             <FormGroup
                                 helperText="Check your RDS or RDS manual."
-                                label="Clicks per MOA."
-                                labelFor="clicksPerMOA"
+                                label={`Clicks per MOA.`}
                                 labelInfo="(required)"
                             >
                                 <NumericInput
-                                    onValueChange={(val) =>
-                                        setClicksPerMOA(val)
-                                    }
-                                    // fill={true}
+                                    onValueChange={setClicksPerMOA}
                                     leftIcon={<Icon icon="numerical" />}
                                     placeholder="1 click per MOA, maybe?"
                                     title="1 click per MOA, maybe?"
+                                    min={0}
                                 />
                             </FormGroup>
                             <FormGroup
-                                helperText="How far is your target? (in yards)"
+                                helperText={`Feet: ${otherValuesToTarget.feet}, Meters: ${otherValuesToTarget.meter}`}
                                 label="Yards to target"
-                                labelFor="yardsToTarget"
                                 labelInfo="(required)"
                                 style={{ marginLeft: '10px' }}
                             >
                                 <NumericInput
                                     title="Please provide yards to target, Note: YARDS."
                                     placeholder="Just the number"
-                                    onValueChange={(val) =>
-                                        setYardsToTarget(val)
-                                    }
+                                    onValueChange={_yardsToTarget}
+                                    min={0}
                                     leftIcon={<Icon icon="numerical" />}
                                 />
                             </FormGroup>
@@ -130,49 +166,84 @@ function App() {
                         <ControlGroup fill={true} vertical={false}>
                             <FormGroup
                                 label="Inches off target"
-                                labelFor="inchesOffTarget"
                                 labelInfo="(required)"
-                                // fill={true}
                             >
                                 <NumericInput
                                     title="Provide positive value whether it be elevation/windage."
                                     placeholder="Provide positive value whether it be elevation/windage."
                                     fill={true}
                                     leftIcon={<Icon icon="numerical" />}
+                                    min={0}
                                     onValueChange={(val) =>
-                                        calculateClicks(val)
+                                        setinchesOffTarget(val)
                                     }
                                 />
                             </FormGroup>
                         </ControlGroup>
-                        <FormGroup
-                            labelFor="inchesOffTarget"
-                            fill={true}
-                            style={{
-                                paddingTop: '10px',
-                                paddingBottom: '10px',
-                            }}
-                        >
+                        <FormGroup fill={true}>
                             <Text style={{ fontSize: '14px' }}>
                                 <Icon icon="calculator" />
-                                &nbsp; {clicks} clicks required!
+                                &nbsp; {clicks} clicks or ~{Math.floor(clicks)}{' '}
+                                clicks.
                             </Text>
                         </FormGroup>
-                        <Text
-                            // fill={true}
+                        <ControlGroup
+                            fill={false}
                             style={{
-                                textAlign: 'center',
-                                fontSize: '12px',
-                                fontWeight: 'bold',
+                                justifyContent: 'center',
                             }}
                         >
-                            Copyright &copy; 2024, Prashant Shrestha
-                        </Text>
+                            <Button
+                                icon="calculator"
+                                text="Calculate"
+                                style={{ outline: 'none' }}
+                                onClick={calculateClicks}
+                                fill={true}
+                            />
+                            {/* <Button
+                                icon="refresh"
+                                text="Reset"
+                                style={{ outline: 'none', marginLeft: '10px' }}
+                                onClick={resetFields}
+                            /> */}
+                        </ControlGroup>
+                        <ControlGroup
+                            fill={true}
+                            style={{
+                                marginTop: '20px',
+                                flexDirection: 'column',
+                            }}
+                        >
+                            <Text
+                                style={{
+                                    textAlign: 'center',
+                                    fontSize: '12px',
+                                    fontWeight: 'bold',
+                                    width: '100%',
+                                }}
+                            >
+                                Made with {<Icon icon="heart" color="red" />} in
+                                Virginia.
+                            </Text>
+                            <Text
+                                style={{
+                                    textAlign: 'center',
+                                    fontSize: '10px',
+                                    fontWeight: 'bold',
+                                    marginTop: '10px',
+                                }}
+                            >
+                                Copyright &copy; 2024,{' '}
+                                <a href="https://prashant.me">
+                                    Prashant Shrestha
+                                </a>
+                            </Text>
+                        </ControlGroup>
                     </SectionCard>
                 </Section>
             </div>
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
